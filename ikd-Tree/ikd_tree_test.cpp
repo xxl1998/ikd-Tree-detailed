38102d9de3bc2b08a631da3dae8adeef9ace7d43
xxl1998 <50321124+xxl1998@users.noreply.github.com>
2024-05-03 18:46:45 +0800

    remove pcl_viewer, add ros launch file

--- ikd-Tree/ikd_tree_test.cpp
+++ ikd-Tree/ikd_tree_test.cpp
@@ -1,6 +1,7 @@
 /*
     Description: An example to introduce box search and radius search using ikd-Tree
     Author: Hyungtae Lim, Yixi Cai
+    Modified by xxl1998 May,2024
 */
 #include "ikd_Tree.h"
 #include <stdio.h>
@@ -21,24 +22,6 @@
 using PointType = pcl::PointXYZ;
 using PointVector = KD_TREE<PointType>::PointVector;
 template class KD_TREE<pcl::PointXYZ>;
-
-void colorize( const PointVector &pc, pcl::PointCloud<pcl::PointXYZRGB> &pc_colored, const std::vector<int> &color) {
-    int N = pc.size();
-
-    pc_colored.clear();
-    pcl::PointXYZRGB pt_tmp;
-
-    for (int i = 0; i < N; ++i) {
-        const auto &pt = pc[i];
-        pt_tmp.x = pt.x;
-        pt_tmp.y = pt.y;
-        pt_tmp.z = pt.z;
-        pt_tmp.r = color[0];
-        pt_tmp.g = color[1];
-        pt_tmp.b = color[2];
-        pc_colored.points.emplace_back(pt_tmp);
-    }
-}
 
 KD_TREE<PointType> ikdtree;
 int main(int argc, char **argv) {
@@ -79,19 +62,6 @@ int main(int argc, char **argv) {
     duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
     printf("Search nearest point time cost is %0.3f ms\n",float(duration)/1e3);
 
-    /*** Below codes are just for visualization */
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr src_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr searched_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
-
-    pcl::visualization::PointCloudColorHandlerGenericField<PointType> src_color(src, "x");
-    colorize(search_result, *searched_colored, {255, 0, 0});
-
-    pcl::visualization::PCLVisualizer viewer0("Nearest Search");
-    viewer0.addPointCloud<PointType>(src,src_color, "src");
-    viewer0.addPointCloud<pcl::PointXYZRGB>(searched_colored, "searched");
-    viewer0.setCameraPosition(-5, 30, 175,  0, 0, 0, 0.2, -1.0, 0.2);
-    viewer0.setSize(1600, 900);
-
     ros::Publisher pub_pc_map = nh.advertise<sensor_msgs::PointCloud2>(
         "/pc_map", 1);
     sensor_msgs::PointCloud2 msg_pc_map;
@@ -105,10 +75,5 @@ int main(int argc, char **argv) {
         r.sleep();
         ros::spinOnce();
     }
-
-    while (!viewer0.wasStopped() ){
-        viewer0.spinOnce();
-    }
-
     return 0;
 }
